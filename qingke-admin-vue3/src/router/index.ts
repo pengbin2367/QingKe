03e74eefe07a8f27ca1a90e0682b1bff17bb13db
PengBin <pengbin2367@163.com>
2024-06-01 22:37:38 +0800

    feat: 登陆注册静态页面

--- qingke-admin-vue3/src/router/index.ts
+++ qingke-admin-vue3/src/router/index.ts
@@ -1,11 +1,17 @@
 import {createRouter, createWebHistory, Router, RouteRecordRaw} from "vue-router";
 import Home from "../views/Home.vue";
+import LoginRegister from "../views/LoginRegister.vue";
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginRegister
     }
 ]
 
