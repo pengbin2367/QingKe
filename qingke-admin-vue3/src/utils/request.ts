3f33f14ddb3713b223a01c8e79b5aeba9c219846
PengBin <pengbin2367@163.com>
2024-06-02 18:52:42 +0800

    fix: ElMessage

--- qingke-admin-vue3/src/utils/request.ts
+++ qingke-admin-vue3/src/utils/request.ts
@@ -1,22 +1,27 @@
-import axios from "axios"
-import { Message } from "element-plus";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios"
+
+interface ResponseData<T = any> {
+    code: number;
+    data?: T;
+    message?: string;
+}
 
 const service = axios.create({
-    baseURL: process.env.VUE_APP_BASE_API,
+    // baseURL: process.env.VUE_APP_BASE_API,
     timeout: 5000
 })
 
 service.interceptors.request.use(
-    (config) => {
+    (config: AxiosRequestConfig) => {
         return config
     },
-    (error) => {
+    (error: AxiosError) => {
         Promise.reject(error)
     }
 )
 
 service.interceptors.response.use(
-    (response) => {
+    (response: AxiosResponse<ResponseData>) => {
         // Some example codes here:
         // code == 20000: success
         // code == 50001: invalid access token
@@ -27,18 +32,20 @@ service.interceptors.response.use(
         // You can change this part for your own usage.
         const res = response.data
         if (res.code !== 20000) {
-            Message({
+            ElMessage({
                 message: res.message || 'Error',
                 type: 'error',
                 duration: 5 * 1000
             })
+            return Promise.reject(new Error(res.message || 'Error'))
         } else {
             return response.data
         }
     },
-    (error) => {
-        Message({
-            message: error.message,
+    (error: AxiosError) => {
+        const message = error.response?.data?.message || error.message;
+        ElMessage({
+            message: message,
             type: 'error',
             duration: 5 * 1000
         })
